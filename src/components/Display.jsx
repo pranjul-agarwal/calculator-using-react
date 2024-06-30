f1544ee7e16215191c060d5801af14329be21bed
pranjul agarwal <agarwalpranjul353@gmail.com>
2024-06-30 15:05:16 +0530

    calculator using react.js completed

---
+++ src/components/Display.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from "../styles/Display.module.css";
+const Display = () => {
+  return (
+    <input className={styles.display} type="text" />
+  )
+}
+
+export default Display
